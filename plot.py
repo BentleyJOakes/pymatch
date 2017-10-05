0b303b889457f1960f912a3c94a227e6445a900a
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-10-05 14:33:44 -0400

    Massively change matching times box plot.

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
 from collections import defaultdict
 import os
 
@@ -9,6 +10,14 @@ class Plotter:
         self.decompose_times = defaultdict(list)
         self.old_matching_times = defaultdict(list)
         self.new_matching_times = defaultdict(list)
+
+        self.old_matcher_colour = '#dd2233'
+        self.old_matcher_colour2 = "0.75"
+
+        self.new_matcher_colour = "#2233dd"
+        self.new_matcher_colour2 = "1.00"
+
+        self.max_time_line = "#333333"
 
     def plot_times(self, times_file, old_times, new_times):
 
@@ -36,6 +45,7 @@ class Plotter:
         plt.boxplot(data, labels = labels, showmeans = True, meanline = True)
 
         plt.ylabel('Time (s)')
+        plt.yscale('log')
         plt.title("Times for: " + name)
         #plt.show()
 
@@ -48,6 +58,7 @@ class Plotter:
 
 
     def plot_decompose_times(self):
+        print("Creating decompose time graph...")
         name = "decompose_times"
         labels = sorted(self.decompose_times.keys())
         plt.figure()
@@ -61,27 +72,61 @@ class Plotter:
         plt.close()
 
     def plot_matching_times(self):
+        print("Creating matching time graph...")
         name = "matching_times"
-        labels = []
-        for key in sorted(self.new_matching_times.keys()):
-            labels.append("Old-" + str(key))
-            labels.append("New-" + str(key))
 
         fig = plt.figure()
         fig.set_size_inches(18.5, 10.5)
-        
-        data = []
-        for k in sorted(self.old_matching_times.keys()):
-            data.append(self.old_matching_times[k])
-            data.append(self.new_matching_times[k])
+        i = 0.5
+        for k in sorted(self.new_matching_times.keys()):
+            data = [self.old_matching_times[k], self.new_matching_times[k]]
 
-        plt.boxplot(data, labels = labels, showmeans = True, meanline = True)
+            dividing_line = i+1.5
+            plt.axvline(dividing_line, ls = "-", color = "0.75")
+
+            bp = plt.boxplot(data, positions = [i, i+1], widths = 0.5, sym = '+')
+            self.setBoxColors(bp)
+            i+=2
+        # labels = []
+        # for key in sorted(self.new_matching_times.keys()):
+        #     labels.append("Old-" + str(key))
+        #     labels.append("New-" + str(key))
+        #
+        #
+        #
+        # data = []
+        # for k in sorted(self.old_matching_times.keys()):
+        #     data.append(self.old_matching_times[k])
+        #     data.append(self.new_matching_times[k])
+        #
+        # plt.boxplot(data, labels = labels, showmeans = True, meanline = True)
+        ax = plt.axes()
+
+        keys = [""] + sorted(self.new_matching_times.keys())
+        ax.set_xticklabels(keys)
+
+        m = 0
+        xticks = [0] + [t+m for t in range(1, len(keys) * 2 -1, 2)]
+        ax.set_xticks(xticks)
+
+        # draw temporary red and blue lines and use them to create a legend
+        hB, = plt.plot([1, 1], color=self.old_matcher_colour)
+        hR, = plt.plot([1, 1], color=self.new_matcher_colour)
+        hMAX = plt.hlines(y=300, xmin = 0, xmax=len(keys) * 2, color = self.max_time_line, linestyles = 'dashed')
+        plt.legend((hB, hR, hMAX), ('Old Matcher', 'New Matcher', 'Cutoff Time'), bbox_to_anchor=(0.98, 0.15))
+        hB.set_visible(False)
+        hR.set_visible(False)
+
         plt.xlabel('Graph size')
         plt.ylabel('Time (s)')
+        plt.yscale('log')
+        # ml = MultipleLocator(2.5)
+        # plt.axes().xaxis.set_minor_locator(ml)
+        # plt.gca().xaxis.grid(True, which='minor')
         plt.title("Time for Matching")
 
         results_dir = "results/"
-        plt.savefig(results_dir + "/abc_" + name + '.png', bbox_inches = 'tight', dpi = 100)
+        plt.savefig(results_dir + "/abc_" + name + '.png', bbox_inches = 'tight', dpi = 120)
         plt.close()
 
     def parse_line(self, line):
@@ -115,9 +160,25 @@ class Plotter:
                 self.old_matching_times[size] += old_times
                 self.new_matching_times[size] += new_times
 
-                self.plot_times(times_file, old_times, new_times)
+                #self.plot_times(times_file, old_times, new_times)
 
+    # function for setting the colors of the box plots pairs
+    def setBoxColors(self, bp):
+        plt.setp(bp['boxes'][0], color = self.old_matcher_colour)
+        plt.setp(bp['caps'][0], color = self.old_matcher_colour)
+        plt.setp(bp['caps'][1], color = self.old_matcher_colour)
+        plt.setp(bp['whiskers'][0], color = self.old_matcher_colour)
+        plt.setp(bp['whiskers'][1], color = self.old_matcher_colour)
+        plt.setp(bp['fliers'][0], markeredgecolor = self.old_matcher_colour)
+        plt.setp(bp['medians'][0], color = self.old_matcher_colour)
 
+        plt.setp(bp['boxes'][1], color = self.new_matcher_colour)
+        plt.setp(bp['caps'][2], color = self.new_matcher_colour)
+        plt.setp(bp['caps'][3], color = self.new_matcher_colour)
+        plt.setp(bp['whiskers'][2], color = self.new_matcher_colour)
+        plt.setp(bp['whiskers'][3], color = self.new_matcher_colour)
+        plt.setp(bp['fliers'][1], markeredgecolor = self.new_matcher_colour)
+        plt.setp(bp['medians'][1], color = self.new_matcher_colour)
 
 
 if __name__ == "__main__":
@@ -125,5 +186,5 @@ if __name__ == "__main__":
     times_dir = "./results/times/"
     plotter = Plotter()
     plotter.load_times_dir(times_dir)
-    plotter.plot_decompose_times()
+    # plotter.plot_decompose_times()
     plotter.plot_matching_times()
